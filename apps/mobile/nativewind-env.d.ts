c39261cc1e3c9d6241c4682c212c15023640bddc
nazunanana <azukiii1953@gmail.com>
2026-07-06 22:42:02 +0900

    プロジェクト作成画面UI仮配置

--- apps/mobile/nativewind-env.d.ts
+++ apps/mobile/nativewind-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="nativewind/types" />
 
 declare module '*.css';
+
+declare module '*.png' {
+  const value: number;
+  export default value;
+}
--- apps/mobile/src/app/project/create.tsx
+++ apps/mobile/src/app/project/create.tsx
@@ -2,31 +2,46 @@ import { Button } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
 import { useCreateProject } from '@/hooks/useCreateProject';
 import { router } from 'expo-router';
-import { ScrollView, View } from 'react-native';
+import { Image, ScrollView, View } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import logo from "../../../assets/images/chainclip_logo.png";
 
 export default function CreateScreen() {
   const createProject = useCreateProject();
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <ScrollView contentContainerClassName="px-12 py-8">
-      {/* 画面名 */}
-      <Text className="text-center text-lg font-bold text-gray-700 my-12">
-        このへんにロゴ
-      </Text>
+    <SafeAreaView className="w-full flex-1 bg-white">
+      <ScrollView contentContainerClassName="px-12 py-8 items-center">
+        {/* ロゴ */}
+        <Image source={logo} className="w-64 h-64" resizeMode="contain" />
 
-      {/* シーン生成(解析開始)ボタン */}
-      <View className="w-full gap-3">
-        <Button onPress={() => router.push({
+        <Text className="text-center text-2xl font-bold text-[#029FFF] mt-8 mb-12">
+          思い出の動画をまとめよう
+        </Text>
+
+        <View className="gap-4 my-12">
+          {/* 動画アップロードボタン */}
+          <Button className="py-16 self-center px-4 gap-1 flex flex-col justify-center items-center" onPress={() => router.push({
+            pathname: '/project/[id]/scenes',
+            params: { id: '123' },
+          })}>
+            <MaterialCommunityIcons name="upload" size={68} color="white" />
+            <Text className="text-lg">動画をアップロード</Text>
+          </Button>
+          <Text className="text-xs text-gray-500">ファイルサイズは1アイテムあたり〇GBまでです。</Text>
+        </View>
+
+        {/* 仮ページ遷移ボタン */}
+        <Button className="mb-4" onPress={() => router.push({
           pathname: '/project/[id]/scenes',
           params: { id: '123' },
         })}>
-          <Text>動画をアップロード</Text>
+          <Text className="text-lg">(仮)シーン選択画面へ</Text>
         </Button>
-      </View>
 
-      {/* プロジェクト生成ボタン */}
-      {/* <View className="w-full gap-3">
+        {/* プロジェクト生成ボタン */}
+        {/* <View className="w-full gap-3">
         <Button
           variant="outline"
           onPress={() => createProject.mutate({ aspectRatio: '9:16' })}>
